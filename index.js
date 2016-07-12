b63ed6dd4e6c133ad4aaf99c8de41a655df3beb4
flynnp25 <patrick.flynn25@mail.dcu.ie>
2016-07-12 21:14:23 +0100

    Added more irish Rail Api calls

--- index.js
+++ index.js
@@ -33,3 +33,61 @@ exports.getAllStationsByTypeJSON = function (type, callback) {
         }
     });
 };
+
+exports.getCurrentTrains = function (callback) {
+    request('http://api.irishrail.ie/realtime/realtime.asmx/getCurrentTrainsXML', function (error, response, body) {
+        return callback(body);
+    });
+};
+
+exports.getCurrentTrainsJSON = function (callback) {
+    request('http://api.irishrail.ie/realtime/realtime.asmx/getCurrentTrainsXML', function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            parseString(body, function (err, result) {
+                return callback(result);
+            });
+        }
+    });
+};
+
+exports.getCurrentTrainsByType = function (type,callback) {
+    request('http://api.irishrail.ie/realtime/realtime.asmx/getCurrentTrainsXML_WithTrainType?TrainType='+type, function (error, response, body) {
+        return callback(body);
+    });
+};
+
+exports.getCurrentTrainsByTypeJSON = function (type,callback) {
+    request('http://api.irishrail.ie/realtime/realtime.asmx/getCurrentTrainsXML_WithTrainType?TrainType='+type, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            parseString(body, function (err, result) {
+                return callback(result);
+            });
+        }
+    });
+};
+
+exports.getStationData = function (params,callback) {
+    var url = 'http://api.irishrail.ie/realtime/realtime.asmx/getStationDataByNameXML';
+
+    if(params.hasOwnProperty('NumMins')){
+        url = url + '_withNumMins';
+    }
+
+    console.log(url);
+    request({
+        url: url,
+        method: 'POST',
+        headers: {
+            'Content-Type': 'MyContentType',
+            'Custom-Header': 'Custom Value'
+        },
+        form: params
+    }, function(error, response, body){
+        if(error) {
+            console.log(error);
+            callback(error);
+        } else {
+            callback(body);
+        }
+    });
+};
